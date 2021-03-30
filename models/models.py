f5e91fe52986e308afc12dc0d3b293907bf2ad52
Genmai <hotaru103bsb@gmail.com>
2021-03-30 11:59:32 +0900

    classの先頭を大文字にしました

--- models/models.py
+++ models/models.py
@@ -4,7 +4,7 @@ from models.database import Base
 from datetime import datetime
 
 
-class questions(Base):
+class Questions(Base):
     __tablename__ = 'questions'
     id = Column(Integer, primary_key=True,autoincrement = True)
     title = Column(String(128), unique=True)
@@ -20,7 +20,7 @@ class questions(Base):
     def __repr__(self):
         return '<Title %r>' % (self.title)
 
-class comments(Base):
+class Comments(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True,autoincrement = True)
     question_id = Column( 'question_id',Integer,ForeignKey('questions.id', onupdate='CASCADE', ondelete='CASCADE'))
